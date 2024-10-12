43bee1633b797a0d6ceafcd6b9dfde49d908e817
Dhaval Prajapati <pdhaval2705@gmail.com>
2024-10-12 21:27:11 +0530

    12-oct

--- OneDrive/Desktop/codes/Webd project/app.js
+++ OneDrive/Desktop/codes/Webd project/app.js
@@ -1,5 +1,17 @@
 const login = document.getElementById("login-page");
 const login_ahref = document.getElementById("login-src")
-if (screen.width< 700) {
-    login_ahref.href="login.html"
-}
+
+onload = fetch("./Book details.csv").then(res => {
+    return res.text();
+}).then(data => {
+     let result = data.split(/\r?\n|\r/).map(e => {
+        return e.split(",")
+     })
+    result.forEach(r => {
+       console.log(r[5])
+    })
+    console.log(result)
+})
+
+
+
